f6f0030ac17843f03d39ac8c522495443f5b4593
Eduardo Berlatto <eduardoberlatto@gmail.com>
2024-03-27 23:55:43 -0300

    Segundo dia

--- src/main/java/me/dio/sdw24/domain/model/Champion.java
+++ src/main/java/me/dio/sdw24/domain/model/Champion.java
@@ -9,5 +9,12 @@ public record Champion(
         String lore,
         String imageUrl
 ) {
-
+    public String generateContextByQuestion(String question) {
+        return """
+                Pergunta: %s
+                Nome do Campeão: %s
+                Função: %s
+                Lore (História): %s
+                """.formatted(question, this.name, this.role, this.lore);
+    }
 }
